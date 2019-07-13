a0cae1b5c5f41be90b25772601da9316d4df4231
Jose Hidalgo <jmhc1995@hotmail.com>
2019-07-13 17:51:21 -0600

    lab administrador muestra dispositivos dinamicamente

--- src/app/lab-administrator/lab-administrator.component.ts
+++ src/app/lab-administrator/lab-administrator.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lab-administrator.component.scss']
 })
 export class LabAdministratorComponent implements OnInit {
+  chosenType = 0;
+  currentDevice = 0;
+  deviceType = [
+      'Computadora',
+      'Video Beam'
+  ];
+
+  devices = [
+      '#178485',
+      '#278485',
+      '#378485',
+      '#478485',
+      '#578485',
+      '#678485',
+      '#778485',
+      '#878485',
+      '#978485',
+      '#188485',
+      '#288485',
+      '#388485',
+      '#488485',
+      '#588485',
+      '#688485',
+      '#788485'
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setChosenType(index) {
+    this.chosenType = index;
   }
 
 }
