59d22a5dccef61127ae399091454fb36c79eb14e
Jose Hidalgo <jmhc1995@hotmail.com>
2019-07-13 22:40:28 -0600

    inicio administrador edificio viejo casi listo (en vista y dinamicamente) falta pop up de agregar

--- src/app/inicio-administrator/inicio-administrator.component.ts
+++ src/app/inicio-administrator/inicio-administrator.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioAdministratorComponent implements OnInit {
 
+  labs = [
+    {numer: 102, capacidad: 25, estado: 0},
+    {numer: 104, capacidad: 25, estado: 0},
+    {numer: 105, capacidad: 25, estado: 1},
+    {numer: 106, capacidad: 25, estado: 0},
+    {numer: 107, capacidad: 25, estado: 2}
+  ];
+
+  estado = [
+      'btn btn-success',
+      'btn btn-danger',
+      'btn btn-warning',
+  ]
+
   constructor() { }
 
   ngOnInit() {
